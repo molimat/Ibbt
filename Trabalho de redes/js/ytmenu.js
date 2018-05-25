c165aea41c6782a9da56722091ea39d8acf0a133
Molimat <matheusmolin@poli.ufrj.br>
2018-05-24 22:16:49 -0300

    Site_v1

---
+++ Trabalho de redes/js/ytmenu.js
@@ -0,0 +1,67 @@
+
+var YTMenu = (function() {
+
+	function init() {
+		
+		[].slice.call( document.querySelectorAll( '.dr-menu' ) ).forEach( function( el, i ) {
+
+			var trigger = el.querySelector( 'div.dr-trigger' ),
+				icon = trigger.querySelector( 'span.dr-icon-menu' ),
+				open = false;
+
+			trigger.addEventListener( 'click', function( event ) {
+				if( !open ) {
+					el.className += ' dr-menu-open';
+					open = true;
+				}
+			}, false );
+
+			icon.addEventListener( 'click', function( event ) {
+				if( open ) {
+					event.stopPropagation();
+					open = false;
+					el.className = el.className.replace(/\bdr-menu-open\b/,'');
+					return false;
+				}
+			}, false );
+
+		} );
+
+	}
+
+	init();
+
+})();
+
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {myFunction()};
+
+// Get the header
+var header = document.getElementById("myHeader");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky  ) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+let anchorlinks = document.querySelectorAll('a[href^="#"]')
+ 
+for (let item of anchorlinks) { // relitere 
+    item.addEventListener('click', (e)=> {
+        let hashval = item.getAttribute('href')
+        let target = document.querySelector(hashval)
+        target.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        })
+        history.pushState(null, null, hashval)
+        e.preventDefault()
+    })
+}
